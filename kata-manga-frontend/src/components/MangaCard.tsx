fc2c9d371a2d0b3a06fc5ee0f7051e58816754a1
JaavLex <alexandre.javet@hotmail.com>
2022-06-22 20:50:06 +0200

    [feat] Created Mangadetail page

---
+++ kata-manga-frontend/src/components/MangaCard.tsx
@@ -0,0 +1,100 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+type MangaCardProps = {
+	imageUrl: string;
+	title: string;
+	volumes: number;
+	chapters: number;
+	status: string;
+	published: string;
+	genres: string[];
+	authors: string[];
+	magazines: string[];
+	synopsis: string;
+};
+
+export default function MangaCard({
+	imageUrl,
+	title,
+	volumes,
+	chapters,
+	status,
+	published,
+	genres,
+	authors,
+	magazines,
+	synopsis,
+}: MangaCardProps) {
+	return (
+		<Box
+			display="flex"
+			flexDirection="row"
+			justifyContent="center"
+			alignItems="center"
+			gap={8}
+		>
+			<Box>
+				<img src={imageUrl} />
+			</Box>
+			<Box
+				display="flex"
+				flexDirection="column"
+				justifyContent="start"
+				alignItems="start"
+				maxWidth="600px"
+				flexWrap="wrap"
+			>
+				<Typography variant="h4">{title}</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Volumes: {volumes}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Chapters: {chapters}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Status: {status}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Published: {published}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Genres: {genres.map(g => ' ' + g).join(', ')}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Authors: {authors.map(g => ' ' + g).join(', ')}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="subtitle1" fontWeight="bold">
+						Magazines: {magazines.map(g => ' ' + g).join(', ')}
+					</Typography>
+				</Typography>
+				<Typography component="span">
+					<Typography variant="h4" fontWeight="bold">
+						Synopsis
+					</Typography>
+					<Typography
+						variant="body1"
+						fontWeight="bold"
+						maxWidth="600px"
+						style={{ wordWrap: 'break-word' }}
+					>
+						{synopsis}
+					</Typography>
+				</Typography>
+			</Box>
+		</Box>
+	);
+}
--- kata-manga-frontend/src/pages/App.tsx
+++ kata-manga-frontend/src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Layout from './Layout';
+import MangaDetail from './MangaDetail';
 import Mangas from './Mangas';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />} />
 					<Route path="mangas" element={<Mangas />} />
+					<Route path="mangadetail" element={<MangaDetail />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
